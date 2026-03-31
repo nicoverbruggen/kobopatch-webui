77597b6997497388fede75c12e53203700648d8e
Nico Verbruggen <mail@nicoverbruggen.be>
2026-03-31 23:18:54 +0200

    Update description

--- web/src/nickelmenu/features/exclude-calibre/index.js
+++ web/src/nickelmenu/features/exclude-calibre/index.js
@@ -1,6 +1,6 @@
 export default {
     id: 'exclude-calibre',
-    title: 'Ignore Calibre folder',
-    description: 'Prevents new books in the `calibre` folder from showing up in Kobo\'s list of books. Enable this if you plan on using KOReader to read those books.',
+    title: 'Prevent Calibre books from appearing in My Books',
+    description: 'Prevents new books in the "calibre" folder from being detected and added to "My Books". Enable this if you plan on using KOReader to read those books and want books transferred via Calibre and purchased books to remain separate.',
     default: false,
 };
